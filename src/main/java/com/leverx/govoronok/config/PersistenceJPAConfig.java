3356d0ed6c1674f006476f08fc69616de5109db3
Miwoder <bestboy75@rambler.ru>
2021-11-26 23:26:35 +0300

    correct config with application properties

--- src/main/java/com/leverx/govoronok/config/PersistenceJPAConfig.java
+++ src/main/java/com/leverx/govoronok/config/PersistenceJPAConfig.java
@@ -1,7 +1,10 @@
 package com.leverx.govoronok.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,7 +21,16 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.leverx.govoronok.repository"})
+@PropertySource("classpath:application.properties")
 public class PersistenceJPAConfig {
+
+    private Environment environment;
+
+    @Autowired
+    public PersistenceJPAConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em
@@ -36,10 +48,10 @@ public class PersistenceJPAConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/LeverProject");
-        dataSource.setUsername( "root" );
-        dataSource.setPassword( "root" );
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driver"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
         return dataSource;
     }
 
